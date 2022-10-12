deb78a25f67aa41364358fa95ceef0bdcefb47a7
11m245 <94919109+11m245@users.noreply.github.com>
2022-10-12 12:22:31 +0530

    Add files via upload
    getelementbyID_sTask

---
+++ value.js
@@ -0,0 +1,13 @@
+function display(){
+
+
+    console.log(`fname is ${document.getElementById("fname").value}`);
+    console.log(`mname is ${document.getElementById("mname").value}`);
+    console.log(`lname is ${document.getElementById("lname").value}`);
+    console.log(`Mobile Number is ${document.getElementById("pno").value}`);
+    console.log(`e-mail ID is ${document.getElementById("em").value}`);
+    console.log(`Password is ${document.getElementById("passw").value}`);
+
+
+
+}
